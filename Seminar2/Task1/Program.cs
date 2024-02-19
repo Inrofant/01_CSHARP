32968bf696e28a0b54bd45a5cffc96a9e9e4a258
Dracolis <pkandpy@mail.ru>
2024-02-19 20:53:22 +0300

    add Seminar2 Task 1-4

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход  
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+
+using Internal;
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int s = num/100*10 + num%10;
+Console.WriteLine(s);
---
+++ Seminar2/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит 
+// третью слева цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.Write("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// num < 100 -> третьей цифры нет
+// num = 5789  / 10
+// num = 578
+
+int num2 = 0;
+if(num < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num /= 10; //num / 10
+    }
+    num2 = num % 10;
+    Console.WriteLine(num2);
+}
