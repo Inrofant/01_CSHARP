66af3aaf520f6342e631106f999115b2eb736994
Dracolis <pkandpy@mail.ru>
2024-02-22 23:34:48 +0300

    add Seminar3 and Task1-4

---
+++ Seminar3/Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем генератор случайных чисел
+        Random random = new Random();
+        
+        // Создаем массив из 10 элементов
+        int[] array = new int[10];
+
+        // Заполняем массив случайными числами из промежутка [-10, 10]
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-10, 11); // Генерируем число от -10 до 10 включительно
+        }
+
+        // Выводим исходный массив
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        // Меняем знак у элементов массива
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = -array[i];
+        }
+
+        // Выводим измененный массив
+        Console.WriteLine("\nМассив после замены знаков:");
+        PrintArray(array);
+    }
+
+    // Метод для вывода массива на консоль
+    static void PrintArray(int[] array)
+    {
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Seminar3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив.
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Исходный массив
+        int[] array = {1, 2, 3, 4, 5};
+
+        // Создаем новый массив для записи произведений пар чисел
+        int[] products = new int[array.Length / 2];
+
+        // Проходим по исходному массиву и вычисляем произведения пар чисел
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            products[i] = array[i] * array[array.Length - 1 - i];
+        }
+
+        // Выводим результаты
+        Console.WriteLine("Произведения пар чисел:");
+        foreach (int product in products)
+        {
+            Console.Write(product);
+        }
+    }
+}
